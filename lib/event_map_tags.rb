25051452bcf3d6f140f18ffc43eaabcf2e00ba78
Will <will@spanner.org>
2010-03-17 10:36:03 +0000

    radius tags simpler

--- lib/event_map_tags.rb
+++ lib/event_map_tags.rb
@@ -1,53 +1,33 @@
 module EventMapTags
   include Radiant::Taggable
   class TagError < StandardError; end
-  
+    
   desc %{
-    Renders the tags that will show a google map with markers for all the events in the current list.
-    Unlike other events: tags, the list will not be paginated.
+    Pulls in both the main google map script and the local script that defines markers and zooms the map.
+    To put a map on the page unobtrusively:
+      
+    <pre><code>
+    <head>
+      <r:events:googlemap_javascript />
+    </head>
+    <body>
+      <r:events:googlemap_container />
+    </body>
+    </code></pre>
     
-    You need to make sure there's a gmaps_api_key.yml file in your main application/config directory.
-
-    *Usage:* 
-    <pre><code><r:events:googlemap /></code></pre>
+    Note that this only works on an event calendar page.
+    
   }
-  tag "events:googlemap" do |tag|
-    tag.locals.events ||= get_events(tag, false)
-	  map = GMap.new("events_map")
-    points = []
-    # map.center_zoom_init(["52.4008075","0.2635165"], 8)
-	  map.control_init(:large_map => true,:map_type => true)
-
-    tag.locals.events.each do |event|
-      tag.locals.event = event
-      ll = GLatLng.new([event.lat, event.lng])
-      html = %{
-        <h3 class="event">#{tag.render('event:title')}</h3>
-        <p class="venue">#{tag.render('event:venue')}</p>
-        <p class="description">#{tag.render('event:short_description')}</p>
-      }
-      points.push ll
-      map.overlay_init(GMarker.new(ll, :title => event.title, :info_window => html))
-    end
-
-    lats = points.map(&:lat)
-    lngs = points.map(&:lng)
-	  bounds = [[lats.min, lngs.max], [lats.max, lngs.min]]
-    map.center_zoom_on_bounds_init(bounds)
-    
-    map.div + map.to_html
-  end
-  
-  tag "events:map_header" do |tag|
+  tag "events:googlemap_javascript" do |tag|
     %{
-<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=#{Radiant::Config['event_calendar.google_api_key']}&amp;hl=&amp;sensor=false" type="text/javascript"></script>      
+<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=#{Radiant::Config['event_calendar.google_api_key']}&amp;hl=&amp;sensor=false" type="text/javascript"></script>
+<script type="text/javascript" charset="utf-8" src="#{events_path(url_parts.merge({:format => :js}))}"></script
     }  
   end
   
-  tag "events:unobtrusive_googlemap" do |tag|
-    %{<script type="text/javascript" charset="utf-8" src="#{events_path(url_parts.merge({:format => :js}))}"></script>}  
-  end
-
+  desc %{
+    Renders the empty div that the googlemap will populate.
+  }
   tag "events:googlemap_container" do |tag|
     %{<div id="events_map"></div>}
   end
