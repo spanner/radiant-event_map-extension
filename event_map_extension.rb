9244fbe52a9ca8e8dc8fcdd1f36609f4431c9dd2
spanner <will@spanner.org>
2011-07-05 10:33:10 +0100

    structure updated for radiant 1

--- event_map_extension.rb
+++ event_map_extension.rb
@@ -1,10 +1,12 @@
 # Uncomment this if you reference any of your controllers in activate
 # require_dependency 'application_controller'
 
+require "radiant-event_map-extension"
+
 class EventMapExtension < Radiant::Extension
-  version "1.3.3"
-  description "Small additions to geocode calendar events and display on a map, separated here because only of interest to a few."
-  url "spanner.org"
+  version RadiantEventMapExtension::VERSION
+  description RadiantEventMapExtension::DESCRIPTION
+  url RadiantEventMapExtension::URL
   
   extension_config do |config|
     config.gem "geokit"
@@ -14,7 +16,7 @@ class EventMapExtension < Radiant::Extension
     require 'angle_conversions'           # adds String.to_latlng and some degree/radian conversions to Numeric
     require 'grid_ref'                    # converts from UK grid references to lat/long
     EventVenue.send :include, Mappable    # adds geolocation on validation
-    Page.send :include, EventMapTags      # currently only a very basic events:googlemap tag
+    Page.send :include, EventMapTags      # currently only a basic events:googlemap tag
   end
   
 end
---
+++ lib/radiant-event_map-extension.rb
@@ -0,0 +1,8 @@
+module RadiantEventMapExtension
+  VERSION = '1.3.4'
+  SUMMARY = %Q{Event geocoding and map extension for Radiant CMS}
+  DESCRIPTION = "Small additions to the event_calendar that geocode calendar events and display on a google map. Separated here because perhaps only of interest to a few."
+  URL = "http://github.com/radiant/radiant-event_calendar-extension"
+  AUTHORS = ["William Ross"]
+  EMAIL = ["radiant@spanner.org"]
+end
